71bf2f9f3608ae02cdd42c5afee71993a3a9e4c3
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2023-11-09 23:03:45 +0530

    comments

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+
+//Create an array to store references to the HTML elements representing dice.
 const diceElements = [];
 for (let i = 1; i <= 6; i++) {
+    // Push each element with IDs "die1" through "die6" into the array.
     diceElements.push(document.getElementById(`die${i}`));
 }
-
+// Get references to the roll button and the element to display the total value.
 const rollButton = document.getElementById("roll-button");
 const totalValueElement = document.getElementById("total-value");
 
 rollButton.addEventListener("click", rollDice);
-
+// Function to roll the dice and calculate the total value.
 function rollDice() {
     let totalValue = 0;
 
